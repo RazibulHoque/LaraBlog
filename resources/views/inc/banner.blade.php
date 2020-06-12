1a63e68bc5e275ba7fcccedb6759c1a2f06b2907
Razibul Hoque <47460445+RazibulHoque@users.noreply.github.com>
2020-06-13 02:55:38 +0600

    contact

--- resources/views/inc/banner.blade.php
+++ resources/views/inc/banner.blade.php
@@ -3,8 +3,8 @@
       <div class="hero-banner">
         <div class="hero-banner__content">
           <h3>Tours & Travels</h3>
-          <h1>Amazing Places on earth</h1>
-          <h4>December 12, 2018</h4>
+          <h1> <?php $someText = "Amazing Places on earth";
+          echo $someText ?> </h1>
         </div>
       </div>
     </div>
--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -12,7 +12,7 @@
           <!-- Collect the nav links, forms, and other content for toggling -->
           <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
             <ul class="nav navbar-nav menu_nav justify-content-center">
-              <li class="nav-item active"><a class="nav-link" href="/">Home</a></li> 
+              <li class="nav-item"><a class="nav-link" href="/">Home</a></li> 
               <li class="nav-item"><a class="nav-link" href="/about">About</a></li> 
               <li class="nav-item"><a class="nav-link" href="/">Category</a>
               <li class="nav-item submenu dropdown">
@@ -24,11 +24,11 @@
               </li>
               <li class="nav-item"><a class="nav-link" href="/contact">Contact</a></li>
             </ul>
-            <ul class="nav navbar-nav navbar-right">
+            <ul class="nav navbar-nav navbar-right menu_nav justify-content-end "  >
                 <!-- Authentication Links -->
                 @if (Auth::guest())
-                    <li><a href="{{ route('login') }}" class="text-dark">Login</a></li>
-                    <li><a href="{{ route('register') }}" class="text-dark">Register</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('login') }}" >Login</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('register') }}" >Register</a></li>
                 @else
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
